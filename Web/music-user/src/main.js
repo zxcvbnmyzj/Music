3877eea8546943711ddd6a0709cda223e3a9eb75
zxcvbnmyzj <2750936323@qq.com>
2023-05-22 10:58:53 +0800

    Initialize Project

---
+++ Web/music-user/src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import router from './router'
+import App from './App.vue'
+import ElementUI from 'element-ui'
+
+Vue.config.productionTip = false
+Vue.use(router)
+Vue.use(ElementUI)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
